7df1d9d2bf3fe99507d92ddfc8e19aba3965aeca
Valentin Gorrin <87900039+valentingorr@users.noreply.github.com>
2024-06-10 20:27:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-[[ "$XDG_CONFIG_HOME" != "" ]] || export XDG_CONFIG_HOME=$HOME/.config
-[[ "$XDG_DATA_HOME" != "" ]] || export XDG_DATA_HOME=$HOME/.local/share
-[[ "$XDG_CACHE_HOME" != "" ]] || export XDG_CACHE_HOME=$HOME/.cache
-[[ "$XDG_RUNTIME_DIR" != "" ]] || export XDG_RUNTIME_DIR=/run/user/$(id -u)
+[[ "$XDG_CONFIG_HOME" != "" ]] && export XDG_CONFIG_HOME=$HOME/.config
+[[ "$XDG_DATA_HOME" != "" ]] && export XDG_DATA_HOME=$HOME/.local/share
+[[ "$XDG_CACHE_HOME" != "" ]] && export XDG_CACHE_HOME=$HOME/.cache
+[[ "$XDG_RUNTIME_DIR" != "" ]] && export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 distribution=$(lsb_release -i | awk '{print $3}')
 current_shell=$(echo $SHELL | awk -F '/' '{print $NF}')
